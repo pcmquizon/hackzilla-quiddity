14528a510c2c15eac3cc651b41b651225efd39a8
pcmquizon <camillemendoza17@gmail.com>
2017-03-31 17:14:30 +0800

    remove unused pages

--- src/components/search-select-multi/search-select-multi.ts
+++ src/components/search-select-multi/search-select-multi.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
 
 /*
-  Generated class for the SearchSelectMuli component.
+  Generated class for the SearchSelectMulti component.
 
   See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
   for more info on Angular 2 Components.
 */
 @Component({
-  selector: 'search-select-muli',
-  templateUrl: 'search-select-muli.html'
+  selector: 'search-select-multi',
+  templateUrl: 'search-select-multi.html'
 })
-export class SearchSelectMuliComponent {
+export class SearchSelectMultiComponent {
 
   text: string;
 
   constructor() {
-    console.log('Hello SearchSelectMuli Component');
+    console.log('Hello SearchSelectMulti Component');
     this.text = 'Hello World';
   }
 
